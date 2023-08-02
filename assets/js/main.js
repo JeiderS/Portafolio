eb41541a8ff80f0793a836a9aa5fe1dcf9146eae
JeiderS <140840380+JeiderS@users.noreply.github.com>
2023-08-02 09:32:37 -0500

    Update main.js
    javascrip

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 * Updated: Jul 27 2023 with Bootstrap v5.3.1
 * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
 * Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* License: https://bootstrapmade.com/license
 */
 (function() {
   "use strict";
